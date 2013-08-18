d2b5c3595017bc2aec53587d97654e8fc385605f
Sarath <sarath.hacks@gmail.com>
2013-08-18 10:37:13 +0530

    Created main file
    IP finder main file

---
+++ ipfinder.py
@@ -0,0 +1,23 @@
+#Trinko
+#17/March/2012
+#Sarath
+#IP Tracker Tool
+import socket
+import os
+def trackme():
+	try:
+		url=raw_input("Enter wesite(/eg:google.com)/:");
+		try:
+			ip=socket.gethostbyname(url)
+			print "Ip address of ",url,": ", ip
+		except:
+			print "SysError";
+		raw_input("Press enter to exit")
+		os.system(cmd)
+	except:
+		print "IP Track Tool Connection Error"
+		print "Internet connection not available"
+		print "www.sarathink.wordpress.com"
+		raw_input("Press enter to exit")
+		
+trackme()
